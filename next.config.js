645f8b63a3f7517e03b8786090fabbba2ec09829
Ethan <yanghan0911@gmail.com>
2025-10-24 20:03:00 +0800

    feat: 使用环境变量配置主机名，增强灵活性

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: [process.env.HOST || 'localhost'],
   },
   // 禁用静态页面生成，使用动态渲染
   output: 'standalone',
