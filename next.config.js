e0d2fb84fa6c54dab742b53bf4c9dbd02e6f7f4c
Ethan <yanghan0911@gmail.com>
2025-10-24 20:07:05 +0800

    feat: 更新图片域名配置，支持多个域名

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.HOST || 'localhost'],
+    domains: ['localhost', '0.0.0.0', 'hole.ethan02.com'],
   },
   // 禁用静态页面生成，使用动态渲染
   output: 'standalone',
